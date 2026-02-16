984179d3dbe0b8cf37e355e24a2a3b672e997d15
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-16 23:37:07 +0600

    floating action button add

--- lib/part_2.dart
+++ lib/part_2.dart
@@ -12,44 +12,59 @@ class part2 extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body:Center(
-       child:  Column(
-        children: [
-          
-          ElevatedButton(
-           style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green,
-            foregroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            )
-           ),
-            onPressed: () {
-              print("button pressed");
-            },
-            child: Text("Button",),
-          ),
-          SizedBox(
-            height: 50,
-            width: double.infinity,
-         child:       ElevatedButton(
-           style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green,
-            foregroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            )
-           ),
-            onPressed: () {
-              print("button pressed");
-            },
-            child: Text("Button",),
-          ),
-          ),
-          OutlinedButton(onPressed: (){}, child: Text("outline"))
-        ],
-        
-      ),)
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                print("button pressed");
+              },
+              child: Text("Button"),
+            ),
+            SizedBox(
+              height: 50,
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  print("button pressed");
+                },
+                child: Text("Button"),
+              ),
+            ),
+            OutlinedButton(onPressed: () {}, child: Text("outline")),
+            GestureDetector(
+              onTap: () {
+                print('pressed');
+              },
+              onDoubleTap: () {
+                print('pressed9');
+              },
+              child: Text('this is text'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('floating action button clicked');
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.amber,
+      ),
     );
   }
 }
