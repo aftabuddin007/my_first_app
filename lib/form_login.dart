82ade319f0d1b6b1d87603627576669d4db5f3bf
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-21 16:42:49 +0600

    add validation

--- lib/form_login.dart
+++ lib/form_login.dart
@@ -5,39 +5,74 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    TextEditingController phoneController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
-         backgroundColor: Colors.blueAccent,
-     
+        backgroundColor: Colors.blueAccent,
+
         title: Text("Login Page"),
         centerTitle: true,
-       
       ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             children: [
               Text("Login with your phone number & password"),
-              SizedBox(
-                height: 20,
-
-              ),
-              Form(child:Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text("Phone Number"),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Phone Number",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-
-                      )
+              SizedBox(height: 20),
+              Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text("Phone Number"),
+                    TextFormField(
+                      controller: phoneController,
+                      decoration: InputDecoration(
+                        hintText: "Phone Number",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your phone number";
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
-                  )
-                ],
-              )
-               )
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your password";
+                        } else if (value.length <= 6) {
+                          return "Password must be at least 6 character";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      width: 300,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Submit"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
