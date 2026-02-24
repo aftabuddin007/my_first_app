93b8854036e49d462b4a4e9daa2e4d658a92403e
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-25 00:26:25 +0600

    listtile explore

--- lib/listView.dart
+++ lib/listView.dart
@@ -11,43 +11,60 @@ class ListView2 extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 51, 58, 148),
       ),
-      body: ListView(children: [
-        Container(
-          height: 200,
-          width: 50,
-          color: const Color.fromARGB(255, 161, 181, 33),
-        ),
-        Container(
-          height: 200,
-          width: 50,
-          color: Colors.redAccent,
-        ),
-        Container(
-          height: 200,
-          width: 50,
-          color: const Color.fromARGB(255, 79, 49, 139),
-        ),
-        Container(
-          height: 200,
-          width: 50,
-          color: const Color.fromARGB(255, 47, 157, 13),
-        ),
-        Container(
-          height: 200,
-          width: 50,
-          color: const Color.fromARGB(255, 128, 22, 111),
-        ),
-        Container(
-          height: 200,
-          width: 50,
-          color: const Color.fromARGB(255, 210, 99, 39),
-        ),
-        Container(
-          height: 200,
-          width: 50,
-          color: const Color.fromARGB(255, 41, 179, 137),
-        )
-      ],),
-    );
+      body: ListView.builder(
+        itemCount: 50,
+        itemBuilder: (context, index) {
+         return ListTile(
+          leading: Icon(Icons.phone),
+          trailing: Icon(Icons.delete),
+  title: Text("65577878 $index"),
+         ); 
+          
+          
+          
+        //   return Text("Item $index",style: TextStyle(
+        //     fontSize: 25,
+        //     color: Colors.cyanAccent,
+        //   ),);
+        } 
+        ,
+      //   (children: [
+      //   Container(
+      //     height: 200,
+      //     width: 50,
+      //     color: const Color.fromARGB(255, 161, 181, 33),
+      //   ),
+      //   Container(
+      //     height: 200,
+      //     width: 50,
+      //     color: Colors.redAccent,
+      //   ),
+      //   Container(
+      //     height: 200,
+      //     width: 50,
+      //     color: const Color.fromARGB(255, 79, 49, 139),
+      //   ),
+      //   Container(
+      //     height: 200,
+      //     width: 50,
+      //     color: const Color.fromARGB(255, 47, 157, 13),
+      //   ),
+      //   Container(
+      //     height: 200,
+      //     width: 50,
+      //     color: const Color.fromARGB(255, 128, 22, 111),
+      //   ),
+      //   Container(
+      //     height: 200,
+      //     width: 50,
+      //     color: const Color.fromARGB(255, 210, 99, 39),
+      //   ),
+      //   Container(
+      //     height: 200,
+      //     width: 50,
+      //     color: const Color.fromARGB(255, 41, 179, 137),
+      //   )
+      // ],),
+    ));
   }
 }
